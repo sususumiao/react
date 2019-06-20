46e5f19a6ca43839363bc3196072af06365649a6
sumiao <15976593304@163.com>
2019-06-20 20:35:05 +0800

    优化按钮成单独的类

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
-import React,{Fragment,Component} from 'react';
+import React, { Fragment, Component } from 'react';
 // 接收store中的数据的一个连接器connect
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-class App extends Component{
-  render(){
-    return(
+class PlusButton extends Component {
+  render() {
+    return (
+      <button onClick={this.props.num_add}>+</button>
+    )
+  }
+}
+class SubstrButton extends Component {
+  render() {
+    return (
+      <button onClick={this.props.num_substr}>-</button>
+    )
+  }
+}
+class App extends Component {
+  render() {
+    return (
       <Fragment>
         数据：{this.props.num}
-        <button onClick={this.props.num_add}>+</button>
-        <button onClick={this.props.num_substr}>-</button>
+        <PlusButton {...this.props}></PlusButton>
+        <SubstrButton {...this.props}></SubstrButton>
       </Fragment>
     )
   }
 }
 // store中的数据通过属性 props的方式来使用
 // 成就一个数据映射对象
-const mapStaeToProps = (state)=>{
-  return{
-    num:state.fruitNums
+const mapStaeToProps = (state) => {
+  return {
+    num: state.fruitNums
   }
 }
-const mapDispatch = (dispatch)=>{
-  return{
-    num_add:()=>{
-      const action={
-        type:'num_add',
-        value:1
+const mapDispatch = (dispatch) => {
+  return {
+    num_add: () => {
+      const action = {
+        type: 'num_add',
+        value: 1
       }
       dispatch(action)
     },
-    num_substr:()=>{
-      const action={
-        type:'num_substr',
-        value:1
+    num_substr: () => {
+      const action = {
+        type: 'num_substr',
+        value: 1
       }
       dispatch(action)
     }
   }
 }
-export default connect(mapStaeToProps,mapDispatch)(App);
+export default connect(mapStaeToProps, mapDispatch)(App);
